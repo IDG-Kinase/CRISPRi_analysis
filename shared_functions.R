43634967d6c9a67d9a4d9ae1e2d5cc9be1f085ad
Matthew Berginski <matthew.berginski@gmail.com>
2018-12-17 11:01:42 -0500

    functions for filtering/collating data

---
+++ shared_functions.R
@@ -0,0 +1,22 @@
+calc_reads_percent_per_day <- function(alignments) {
+  alignments = alignments %>%
+    group_by(Day, Clone) %>%
+    summarise(total_reads = sum(NumReads)) %>%
+    select(Day,Clone,total_reads) %>%
+    left_join(alignments) %>%
+    mutate(Percentage_reads = NumReads/total_reads)
+  
+  return(alignments)
+}
+
+ratio_to_day_0 <- function(alignments) {
+  alignments = alignments %>%
+    filter(Day == 0) %>%
+    rename(Percentage_reads_D0 = Percentage_reads) %>%
+    ungroup() %>%
+    select(Name,Percentage_reads_D0) %>%
+    left_join(alignments) %>%
+    mutate(D0_ratio = Percentage_reads/Percentage_reads_D0)
+  
+  return(alignments)
+}
